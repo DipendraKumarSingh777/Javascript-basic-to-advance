7dfb8e42f20417f5c64ec6ecaa5dea444a4e0bca
Dipendra Kumar Singh <140081935+DipendraKumarSingh777@users.noreply.github.com>
2024-07-23 02:33:39 +0530

    loops

---
+++ script.js
@@ -0,0 +1,80 @@
+//for loops
+
+
+
+//console.log("samir singh");
+//console.log("samir singh");
+//console.log("samir singh");
+//console.log("samir singh");
+
+
+
+
+//for (let count=1; count<=5;count++){
+  //  console.log("samir singh");
+//}
+
+
+
+
+// calculate sum of 1 to 5
+//let sum=0;
+//for(let i=1; i<=5; i++){
+  //  sum=sum+i;
+//}
+//console.log("sum=",sum);
+
+
+
+//print i 5 times
+//for (let i=1; i<=5;i++){
+  //  console.log("i=",i);
+//}
+
+//console.log("loop has ended");
+
+
+
+//while loop
+//let i=1;
+//while(i<=5){
+    //console.log("samir singh");
+  //  i++;
+//}
+
+
+//to print atleast one time
+//do while loop
+//let i=20
+//do{
+  //console.log("samir singh");
+ // i++;
+//}while (i<=10);
+
+
+
+//to print 1 to 5 using do while loop
+// let i = 1;
+// do{
+//   console.log("i=",i);
+//   i++;
+// }while (i<=5);
+
+
+
+//print even number between 1 to 10
+// let i =0;
+// for(i=2; i<=10; i++){
+//   if( i % 2===0)
+//     console.log(i);
+// }
+
+//print prime numbers between 1 to 30
+
+let i =0;
+for(i=1; i<=30; i++){
+  if(  i/i)
+    console.log(i);
+}
+
+
