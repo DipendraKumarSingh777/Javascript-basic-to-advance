7f8086c591b9806a209d5288481b419a05849be0
Dipendra Kumar Singh <140081935+DipendraKumarSingh777@users.noreply.github.com>
2024-07-26 19:59:36 +0530

    for of- in loop

---
+++ loops/for-in-loop.js
@@ -0,0 +1,8 @@
+//for in loop
+
+let student={
+  name: "samir singh",
+  age:21,
+  cgpa:8.9,
+  ispass: true,
+};
---
+++ loops/for-of-loop.js
@@ -0,0 +1,9 @@
+// for-of-loop
+
+let str="dipendrasingh";
+let size=0;
+for (let i of str){// iterator
+  console.log("i=",i);
+  size++;
+}
+console.log("string size=",size);//13
