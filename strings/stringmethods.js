0437aaac856a6f8e2e340194d4da94e4d2766f00
Dipendra Kumar Singh <140081935+DipendraKumarSingh777@users.noreply.github.com>
2024-07-26 19:56:54 +0530

    string method

---
+++ strings/stringmethods.js
@@ -0,0 +1,52 @@
+//strings
+
+
+let SpecialString=`hi my name is dipendra`;
+console.log(SpecialString);
+console.log(typeof SpecialString);
+
+
+
+//to uppercase()
+
+// let str=("samir singh");
+// let newStr=str.toUpperCase();
+// console.log(str);
+// console.log(newStr);
+
+
+
+//to lowercase
+
+// let str=("SAMIR SINGH");
+// str = str.toLowerCase();
+// console.log(str);
+
+
+
+// trim case
+
+// let str="     Dipendra singh";
+// console.log(str.trim());//
+
+
+
+//slice case
+let str= "students";
+console.log(str.slice(1,5));//tude
+
+
+
+//concat case
+// let str1="samir";
+// let str2=" singh";
+// let result=str1.concat(str2);
+// console.log(result);
+
+
+
+//another concat case
+let str1="samir";
+let str2=" singh";
+let result="my name is "+str1+str2;
+console.log(result);
