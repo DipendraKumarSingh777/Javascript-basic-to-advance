485133e9b615cce1b8c08e93816b24922b66e1c7
Dipendra Kumar Singh <140081935+DipendraKumarSingh777@users.noreply.github.com>
2024-08-04 04:27:26 +0530

    practice question

---
+++ PRACTICE JAVASCRIPT/string reverse.js
@@ -0,0 +1,7 @@
+//write a function to reverse a string
+
+function reverseString(str){
+    return str.split('').reverse().join('');
+}
+console.log(reverseString('dipendra singh'));//hgnis ardnepid
+
