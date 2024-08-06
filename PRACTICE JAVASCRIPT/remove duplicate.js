2e885e940f2adf67d83d5a4a4f16ace31344ab7e
Dipendra Kumar Singh <140081935+DipendraKumarSingh777@users.noreply.github.com>
2024-08-06 19:40:43 +0530

    reverse

---
+++ PRACTICE JAVASCRIPT/remove duplicate.js
@@ -0,0 +1,5 @@
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+1//
+// function largestNumber(arr) {
+//     return Math.max(...arr);
+// }
+
+// console.log(largestNumber([1, 2, 3, 4, 5, 6, 7, 8])); // 8
+
+
+
+2//
+// function reversestring(str){
+//     return str.split('').reverse().join('');
+// }
+// console.log(reversestring('pranay'));
+
+
+3//
 function isPalindrome(str){
-    const reversed= str.split('').reverse().join('');
-    return str===reversed;
+    const reversed =str.split('').reverse().join('');
+    return str ===reversed;
 }
-console.log (isPalindrome('srkrs'));//true
-console.log(isPalindrome('samir'));//false
+console.log (isPalindrome('dsd'));
