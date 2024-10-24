b2a3b05524027e0e29e9c1f769ac41ebc3c2efc1
Dipendra Kumar Singh <140081935+DipendraKumarSingh777@users.noreply.github.com>
2024-10-24 23:38:55 +0530

    Update practicequestion.js

--- strings/practicequestion.js
+++ strings/practicequestion.js
@@ -1,7 +1,7 @@
 
 
 //prompt the user to enter their full name. generate a username for them based on their input . start username with @ followed by their full name and ending with the full nam e length.
-let fullName=prompt("enter your fullname without spaces");
+let fullName=prompt("enter your Fullname without spaces");
 
 let username="@"+fullName+fullName.length;
 console.log(username);
